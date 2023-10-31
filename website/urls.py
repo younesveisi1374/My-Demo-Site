ff68f1663066b5222da159e9de6b2ce29a4efc9f
younesveisi1374 <younesveisi1374@gmail.com>
2023-10-31 10:36:27 +0330

    created first app

---
+++ website/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from website.views import index_view, about_view, contact_view
+
+urlpatterns = [
+    path("", index_view),
+    path("about", about_view),
+    path("contact", contact_view),
+]
