f7cab6fbf58bfc0e48da97f64da43537130a5d19
younesveisi1374 <younesveisi1374@gmail.com>
2023-10-31 13:41:47 +0330

    add bootstrap to project

--- website/views.py
+++ website/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, HttpResponse
 def index_view(request):
     return render(request, 'website/home.html')
 
+
 def profile_view(request):
     return render(request, 'website/profile.html')
 
